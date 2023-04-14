c1bde0c51f555403082486db558cd179094a0ac3
Sinchana C Y <1ms19ee056@gmail.com>
2023-04-14 13:54:20 +0530

    third_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Login from './Components/Login';
+import NavBar from './Components/NavBar';
+import ForgotPassword from './Components/ForgotPassword';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Register from './Components/Register';
+import SecuredAPI from './Components/SecuredAPI';
+import EmailVerificationToken from './Components/EmailVerificationToken';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<NavBar />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/forgotPassword" element={<ForgotPassword />} />
+      <Route exact path="/securedAPI" element={<SecuredAPI />} />
+      <Route exact path="/emailVerify" element={<EmailVerificationToken />} />
+     </Routes>
+     </Router>
+    </>
   );
 }
 
