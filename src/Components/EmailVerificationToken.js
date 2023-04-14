34f1c1963bf4dd3dcb1b2121923ccbea6ca66e62
Sinchana C Y <1ms19ee056@gmail.com>
2023-04-14 13:22:10 +0530

    second_commit

---
+++ src/Components/EmailVerificationToken.js
@@ -0,0 +1,112 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function EmailVerificationToken() {
+    const navigate = useNavigate();
+    const [validated, setValidated] = useState(false);
+    const [token,setToken]=useState('');
+    const [status,setStatus]=useState('');
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log("hey");
+        const form = event.currentTarget;
+        if (form.checkValidity() === true) {
+          console.log(token);
+          const api=token;
+          console.log(api);
+          
+          try {
+            const response=await axios.get(api,{
+            headers:{
+              'Content-Type':'application/json',
+            },
+            
+          })
+            if(response.data==="User Verified Successfully")
+            {
+                console.log("success");
+                navigate("/login");
+            }
+            else{
+                setStatus("Invalid Token");
+            }
+              console.log(response.data);       
+          
+        }
+          catch (error) {   
+            if(error.message==="Request failed with status code 404") 
+            {
+                setStatus("Not Found Error : 404")
+            }
+            else{
+                setStatus("Network Error")
+            }
+            
+            console.log(error.message);
+          } 
+        }
+        else{
+          event.preventDefault();
+          event.stopPropagation();
+        }
+       setValidated(true)
+    };
+  return (
+    <>
+    
+      <Navbar className="navbar navbar-expand-lg bg-info d-none d-lg-flex noprint justify-content-between">
+      <Container fluid>
+        <Image alt="logo" src="https://d3fufwrs5ttbo7.cloudfront.net/layout-media/logo-eform.svg" className='mx-5' />
+        <Navbar.Brand href="/"><b>Home</b></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-3 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+          </Nav>
+            <Image alt="logo"src="https://d3fufwrs5ttbo7.cloudfront.net/layout-media/irs-logo.png" className='mx-5' />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div>
+      <h4 className='text-center my-3'>Email verification Token</h4>  
+    </div>
+    <Form noValidate validated={validated} onSubmit={handleSubmit} className="mb-3 container">
+        <Form.Group as={Col} md="4" controlId="validationCustomEmail">
+          <Form.Label>Token</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend"></InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="token*"
+              value={token}
+              required
+              onChange={(e)=>setToken(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a Token.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Button className="my-4" type="submit">Verify</Button>
+        <div className=' my-3 container h5'>{status}</div>
+
+        </Form>
+        
+    
+        
+    </>
+  );
+}
